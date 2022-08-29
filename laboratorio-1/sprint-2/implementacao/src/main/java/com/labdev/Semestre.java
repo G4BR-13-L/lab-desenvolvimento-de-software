23008d6e5c413a2e1f19a73abd7afa1f388969fa
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-08-28 21:31:52 -0300

    Adição de ArrayList na classe semestre

--- laboratorio-1/sprint-2/implementacao/src/main/java/com/labdev/Semestre.java
+++ laboratorio-1/sprint-2/implementacao/src/main/java/com/labdev/Semestre.java
@@ -1,14 +1,16 @@
 package com.labdev;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 
 public class Semestre {
     private String id;
     private String nome;
     private LocalDateTime dataInicio;
     private LocalDateTime dataFim;
-
+    private List<Disciplina> disciplinas;
     public Semestre(){
-        
+        this.disciplinas = new ArrayList<>();
     }
 }
