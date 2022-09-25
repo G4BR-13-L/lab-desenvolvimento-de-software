8dca31a5b78df7626a602d4b7071cf822d69dd6e
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2022-09-25 13:53:26 -0300

    Sprint 02: Adicionado o CRUD de usuario utilizando springboot

--- laboratorio-2/sprint-2/implementacao/src/main/java/com/labdev/Main.java
+++
@@ -1,7 +0,0 @@
-package com.labdev;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ laboratorio-2/sprint-2/implementacao/src/main/java/com/labdev/model/User.java
@@ -0,0 +1,27 @@
+package com.labdev.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuarios")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+}
