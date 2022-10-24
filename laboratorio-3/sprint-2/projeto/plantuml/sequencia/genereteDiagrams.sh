1f5ec7d5c00142df64c20f9860ba228520282e59
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-23 23:16:25 -0300

    Todos os diagramas Gerados

--- laboratorio-3/sprint-2/projeto/plantuml/sequencia/genereteDiagrams.sh
+++ laboratorio-3/sprint-2/projeto/plantuml/sequencia/genereteDiagrams.sh
@@ -2,25 +2,21 @@ java -jar plantuml.jar -tpdf US1.plantuml ;
 java -jar plantuml.jar -tpdf US2.plantuml ;
 java -jar plantuml.jar -tpdf US3.plantuml ;
 java -jar plantuml.jar -tpdf US4.plantuml ;
-java -jar plantuml.jar -tpdf US5eUS6.plantuml ;
+java -jar plantuml.jar -tpdf US5.plantuml ;
 java -jar plantuml.jar -tpdf US7.plantuml ;
 java -jar plantuml.jar -tpdf US8.plantuml ;
 java -jar plantuml.jar -tpdf US9.plantuml ;
 java -jar plantuml.jar -tpdf US10.plantuml ;
-java -jar plantuml.jar -tpdf US11.plantuml ;
-java -jar plantuml.jar -tpdf US12.plantuml ;
 java -jar plantuml.jar -tpdf US13.plantuml ;
 
 mv US1.pdf ../../
 mv US2.pdf ../../
 mv US3.pdf ../../
 mv US4.pdf ../../
-mv US5eUS6.pdf ../../
+mv US5eUS6eUS11.pdf ../../
 mv US7.pdf ../../
 mv US8.pdf ../../
 mv US9.pdf ../../
 mv US10.pdf ../../
-mv US11.pdf ../../
-mv US12.pdf ../../
 mv US13.pdf ../../
 
