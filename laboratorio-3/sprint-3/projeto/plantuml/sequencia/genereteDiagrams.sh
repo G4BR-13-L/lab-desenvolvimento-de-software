27b5c8a81930b2dfb4f3a6763461f4cd14430748
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-30 19:26:23 -0300

    Criada pasta da sprint 4

---
+++ laboratorio-3/sprint-3/projeto/plantuml/sequencia/genereteDiagrams.sh
@@ -0,0 +1,22 @@
+java -jar plantuml.jar -tpdf US1.plantuml ;
+java -jar plantuml.jar -tpdf US2.plantuml ;
+java -jar plantuml.jar -tpdf US3.plantuml ;
+java -jar plantuml.jar -tpdf US4.plantuml ;
+java -jar plantuml.jar -tpdf US5.plantuml ;
+java -jar plantuml.jar -tpdf US7.plantuml ;
+java -jar plantuml.jar -tpdf US8.plantuml ;
+java -jar plantuml.jar -tpdf US9.plantuml ;
+java -jar plantuml.jar -tpdf US10.plantuml ;
+java -jar plantuml.jar -tpdf US13.plantuml ;
+
+mv US1.pdf ../../
+mv US2.pdf ../../
+mv US3.pdf ../../
+mv US4.pdf ../../
+mv US5eUS6eUS11.pdf ../../
+mv US7.pdf ../../
+mv US8.pdf ../../
+mv US9.pdf ../../
+mv US10.pdf ../../
+mv US13.pdf ../../
+
