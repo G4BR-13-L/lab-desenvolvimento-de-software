a836d22413d92f37aa624bd093f07b61ebd276bf
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-30 20:42:39 -0300

    Inicializado o projeto em Spring

---
+++ laboratorio-3/sprint-3/implementacao/labdev/src/test/java/com/labdev/labdev/LabdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labdev.labdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
