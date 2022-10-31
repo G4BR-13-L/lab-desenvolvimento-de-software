413f8c739e0a5e2f2c26754981853bc339650ed4
G4BR-13-L <gabriel.victorc13@gmail.com>
2022-10-30 21:16:42 -0300

    Carteira e Carteira Aluno

--- laboratorio-3/sprint-3/implementacao/labdev/src/main/java/com/labdev/labdev/model/Carteira.java
+++ laboratorio-3/sprint-3/implementacao/labdev/src/main/java/com/labdev/labdev/model/Carteira.java
@@ -1,5 +1,32 @@
 package com.labdev.labdev.model;
 
-public class Carteira {
+import java.io.Serializable;
+import java.math.BigInteger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Carteira implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+    private BigInteger saldo;
+
+    public Extrato consultarExtrato(){
+        Extrato e = new Extrato();
+        return e;
+    }
+    private void descontarSaldo(int valor){
+
+    }
+    public BigInteger totalMoedas(){
+        return this.saldo;
+    }
 
 }
