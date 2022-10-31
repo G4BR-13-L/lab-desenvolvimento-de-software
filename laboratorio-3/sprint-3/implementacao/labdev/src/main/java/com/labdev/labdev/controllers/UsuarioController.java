59ab0414d3e704ca1f7524a7993e52bebba0b834
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2022-10-30 21:11:07 -0300

    Adicionado UsuarioController e Views para cadastrar e logar

---
+++ laboratorio-3/sprint-3/implementacao/labdev/src/main/java/com/labdev/labdev/controllers/UsuarioController.java
@@ -0,0 +1,19 @@
+package com.labdev.labdev.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+    @RequestMapping(value = "/cadastrar", method = RequestMethod.GET)
+    public String cadastrar() {
+        return "usuario/cadastrar";
+    }
+
+    @RequestMapping(value = "/logar", method = RequestMethod.GET)
+    public String logdar() {
+        return "usuario/logar";
+    }
+}
